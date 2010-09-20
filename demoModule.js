0cd05f05c7992ba1e02167867a19348c85ea757a
Marak Squires <marak.squires@gmail.com>
2010-09-20 00:57:50 -0400

    [api] async calls work now. response object is passed down the chain to be closed later

--- demoModule.js
+++ demoModule.js
@@ -4,11 +4,13 @@ exports.hello = function(){
   return 'hello world';
 };
 
-exports.asyncHello = function(callback){
+exports.asyncHello = function(callback, res){
   setTimeout(function(){
     console.log('hello world');
     if(typeof callback == 'function'){
-      callback();
+      // callback is going to return a value. if we wanted to continue the chain here, we could pass res
+      var result = callback();
+      res.send(200, {'Content-Type': 'application/json'}, JSON.stringify(result));
     }
   }, 3000);
 }
