5be078c7816b07080811b1f72f1b460409d15a54
Marak Squires <marak.squires@gmail.com>
2011-01-21 17:33:25 -0500

    [misc] Removed in function validation, is now being handled by json-schema

--- examples/demoModule.js
+++ examples/demoModule.js
@@ -1,26 +1,20 @@
 // demo module
 exports.echo = function(options, callback){
-
-  if(!options.msg){
-    callback("missing arguments");
-  }
-
   callback(null, options.msg);
-  
 };
 exports.echo.docs = "this is the echo method, it echos back your msg";
 exports.echo.options = {
   msg: { 
     type: 'string',
     optional: false 
-  },
+  }
 };
 
 exports.ping = function(options, callback){
 
-  callback(null, 'pong');
   setTimeout(function(){
+    callback(null, 'pong');
   }, 2000);
 
 }
-exports.ping.docs = "this is the ping method, it pongs back."
+exports.ping.docs = "this is the ping method, it pongs back.";
